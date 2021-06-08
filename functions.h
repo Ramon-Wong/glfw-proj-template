25fdb80aae411811ef9eab37a3fe8067207b6523
Ramon Wong <rcnwong78@gmail.com>
2021-06-07 21:26:22 -0300

    Create functions.h
    init

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+
+void		Mainloop(GLFWwindow*);
+
+
+
+#endif
